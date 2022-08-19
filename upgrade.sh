8051069ada67ee321239ccbe061819f50378542f
kael <codingtime9@gmail.com>
2022-08-19 20:13:57 +0800

    changes

--- upgrade.sh
+++ upgrade.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
-
 # a script to update and upgrade the system
 # AUR packages are included as well
 
 if [ "$EUID" -ne 0 ]; then
-	echo "root is required"
-	neofetch --logo
-	exit
+	echo 'root is required'
+    if [ "$#" -ne 0 ]; then
+      echo "su -c $1 or sudo $1"
+    fi
+    exit
 fi
+
 
 NORMUSER=$(who | awk 'NR==1{print $1;exit}')
 echo "started at $(date)"
@@ -17,37 +18,62 @@ set -e
 
 if [[ $# -gt 0 ]]; then
 	case $1 in
-		--confirmall|-cf)
+		--confirmall|-ca)
 			pacman -Syu --noconfirm
-			sudo -H -u $NORMUSER bash -c 'paru -Syu --noconfirm'
+			sudo -H -u "$NORMUSER" bash -c 'paru -Syu --noconfirm'
 			pacman -Scc -v --noconfirm
-			sudo -H -u $NORMUSER bash -c 'paru -Sccd -v --noconfirm'
+			sudo -H -u "$NORMUSER" bash -c 'paru -Sccd -v --noconfirm'
 			;;
+    --clean|-c)
+      sudo -H -u "$NORMUSER" bash -c 'paru -Sccd -v --noconfirm'
+      pacman -Scc -v --noconfirm
+      ;;
+    --overwrite|-ov)
+      pacman -Syu --noconfirm --overwrite \*
+			sudo -H -u "$NORMUSER" bash -c 'paru -Syu --noconfirm --overwrite \*'
+			pacman -Scc -v --noconfirm --overwrite \*
+			sudo -H -u "$NORMUSER" bash -c 'paru -Sccd -v --noconfirm --overwrite \*'
+      ;;
 		*)
-			echo "are you sure "$@" is valid?"
-			echo "-cf|--confirmall = to skip the interactive mode."
+			echo "are you sure '$1' is valid?"
+      cat <<- 'EOF'
+available commands:
+-confirmall|-ca # skips the interactive mode
+--clean|-c      # cleans the respective package manager's cache.
+--overwrite|-ov # skips the interactive mode and conflict checks
+EOF
+      echo "usage:
+$0 [-confirmall|-ca]
+$0 [-clean|-c] 
+$0 [--overwrite|-ov]
+"
 			;;
 	esac
 exit
 fi
 
-read -n 1 -p "confirmation to upgrade all packages?[y/n]" PACK1
+read -r -n 1 -p "confirmation to upgrade all packages?[y/n]" PACK1
 printf "\n"
-read -n 1 -p "confirmation to clear all cache(AUR & Pacman)?[y/n]" PACK2
+read -r -n 1 -p "confirmation to clear all cache(AUR & Pacman)?[y/n]" PACK2
 printf "\n"
-
+read -r -n 1 -p "confirmation to overwrite files(to bypass conflict checks)?[y/n]" PACK3
+printf "\n"
 
 if [[ $PACK1 =~ [Yy] ]] ; then
 	pacman -Syu --noconfirm
-	sudo -H -u $NORMUSER bash -c 'paru -Syu --noconfirm'
-	clear
+	sudo -H -u "$NORMUSER" bash -c 'paru -Syu --noconfirm'
 fi
 
 if [[ $PACK2 =~ [Yy] ]] ; then
-	sudo -H -u $NORMUSER bash -c 'paru -Sccd -v --noconfirm'
+	sudo -H -u "$NORMUSER" bash -c 'paru -Sccd -v --noconfirm'
 	pacman -Scc -v --noconfirm
 fi
 
+if [[ $PACK3 =~ [Yy] ]]; then
+  sudo -H -u "$NORMUSER" bash -c 'paru -Syu -v --noconfirm --overwrite'
+	pacman -Syu -v --noconfirm --overwrite
+
+fi
 
 echo "exit code" $?
 exit
