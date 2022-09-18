9e7de7de2dde50013297154eefeed6a0321caddf
kael <codingtime9@gmail.com>
2022-09-18 16:16:17 +0800

    big changes

--- upgradeV2.sh
+++ upgradeV2.sh
@@ -14,47 +14,44 @@ EOF
 echo "  sudo $(basename "$0") -h  # print this out "
 echo "  sudo $(basename "$0") -c  # clear caches "
 echo "  sudo $(basename "$0") -u  # update packages"
+echo "  sudo $(basename "$0") -p  # optional params. eg: --overwrite,etc"
 
 }
 
+
+set -e
 
 USER=$(who | awk 'NR==1{print $1;exit}')
 # paru
-updateparu ()
+updatepkg ()
 {
-  sudo -H -u "$USER" bash -c 'paru -Syu --noconfirm'
+  sh -c "pacman -Syu --noconfirm $*"
+  sudo -H -u "$USER" bash -c "paru -Syu --noconfirm"
 }
 
 
-clearparu ()
+clrpkg ()
 {
-  sudo -H -u "$USER" bash -c 'paru -Sccd -v --noconfirm'
-}
-
-# pacman
-updatepacman ()
-{
-  pacman -Syu --nocomfirm
-}
-
-
-clearpacman ()
-{
-  pacman -Sccd -v --noconfirm
+  sh -c "pacman -Sccd -v --noconfirm $*"
+  sudo -H -u "$USER" bash -c "paru -Sccd -v --noconfirm"
 }
 
 
 UPDATEPKGS=
 CLEARPKGS=
+PARAM=()
 
 # parse args
-while getopts ':huc' arg; do
+while getopts ':hucp:' arg; do
   case "$arg" in
     u)
     UPDATEPKGS=1
     ;;
     c)
     CLEARPKGS=1
+    ;;
+    p)
+    PARAM+=("$OPTARG")
     ;;
     h)
     help
@@ -76,14 +73,16 @@ fi
 
 
 if [[ "$UPDATEPKGS" ]]; then
-  updatepacman
-  updateparu
+  updatepkg "${PARAM[@]}"
+elif [[ "$CLEARPKGS" ]]; then
+  clrpkg "${PARAM[@]}"
 fi
 
-if [[ "$CLEARPKGS" ]]; then
-  clearpacman
-  clearparu
+if [[ -n "${PARAM[*]}" ]]; then
+  echo "optional params ${PARAM[*]}"
 fi
 
 
 shift $(( OPTIND -1 ))
+
+exit $?
